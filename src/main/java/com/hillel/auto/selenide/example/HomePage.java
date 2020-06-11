838b1eb9e0fc471c4edfdcfabef35d4dab539a6a
Alexey Panashchenko <panashchenko89@gmail.com>
2020-06-11 17:39:37 +0300

    Add lecture material

--- src/main/java/com/hillel/auto/selenide/example/HomePage.java
+++ src/main/java/com/hillel/auto/selenide/example/HomePage.java
@@ -2,6 +2,8 @@ package com.hillel.auto.selenide.example;
 
 import com.codeborne.selenide.Condition;
 import io.qameta.allure.Step;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -10,8 +12,11 @@ import static com.codeborne.selenide.Selenide.$;
  */
 public class HomePage {
 
+    static final Logger LOG = LoggerFactory.getLogger(HomePage.class);
+
     @Step("User should be logged in")
     public HomePage userShouldBeLoggedIn(String userName) {
+        LOG.info("User should be logged in");
         $("[href='#@" +userName+"']").shouldBe(Condition.visible.because("User not logged in!"));
         return this;
     }
--- src/main/java/com/hillel/auto/selenide/example/LoginPage.java
+++ src/main/java/com/hillel/auto/selenide/example/LoginPage.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Condition;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.pagefactory.ByChained;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
@@ -12,6 +14,8 @@ import static com.codeborne.selenide.Selenide.$;
  * Created by alpa on 5/10/20
  */
 public class LoginPage {
+
+    static final Logger LOG = LoggerFactory.getLogger(LoginPage.class);
 
     private By authForm = byCssSelector(".auth-page");
     private By loginLink = byAttribute("href", "#login");
@@ -23,13 +27,14 @@ public class LoginPage {
 
     @Step("Open Login page")
     public LoginPage openPage() {
+        LOG.info("Open Login page");
         $(loginLink).shouldBe(Condition.visible).click();
         return this;
     }
 
     @Step("Login as user")
     public HomePage login(String email, String password) {
-        System.out.println("Login as user");
+        LOG.info("Login as user");
         inputEmail(email);
         inputPassword(password);
         return clickSingInButton();
@@ -37,14 +42,15 @@ public class LoginPage {
 
     @Step
     public void inputEmail(String email) {
-        System.out.println("Input email");
+        LOG.info("Input email");
 //        $(authForm).$(emailField).setValue(email);
         $(loginInAuthForm).setValue(email);
     }
 
     @Step
     public void inputPassword(String password) {
-        System.out.println("Input password");
+        LOG.info("Input password");
+        LOG.debug("Input password {}", password);
        $(passwordField).setValue(password);
     }
 
@@ -54,10 +60,9 @@ public class LoginPage {
 
     @Step("Click Sing In button")
     public HomePage clickSingInButton() {
-        System.out.println("Click Sing In button");
+        LOG.info("Click Sing In button");
         $(signInBtn).click();
         return new HomePage();
     }
-
 
 }
