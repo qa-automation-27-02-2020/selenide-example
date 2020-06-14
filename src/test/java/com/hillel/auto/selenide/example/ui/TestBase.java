07cc0af84602a3e334d7a05282091ec09e7cf34d
Alexey Panashchenko <panashchenko89@gmail.com>
2020-06-14 11:41:22 +0300

    Fix allure screenshots

--- src/test/java/com/hillel/auto/selenide/example/ui/TestBase.java
+++ src/test/java/com/hillel/auto/selenide/example/ui/TestBase.java
@@ -3,9 +3,6 @@ package com.hillel.auto.selenide.example.ui;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import io.qameta.allure.selenide.LogType;
-
-import java.util.logging.Level;
 
 /**
  * Created by alpa on 5/24/20
@@ -14,10 +11,10 @@ public class TestBase {
 
     static {
         Configuration.baseUrl = "https://react-redux.realworld.io/";
-//        Configuration.screenshots = false;
+        Configuration.screenshots = true;
         SelenideLogger.addListener("AllureSelenide",
-                new AllureSelenide().screenshots(true)
-                        .savePageSource(true)
-                        .enableLogs(LogType.BROWSER, Level.INFO));
+                new AllureSelenide()
+                        .screenshots(true)
+                        .savePageSource(true));
     }
 }
