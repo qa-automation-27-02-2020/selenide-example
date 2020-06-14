8ec71347e4cd858533f08be04f5b4558a915906f
Alexey Panashchenko <panashchenko89@gmail.com>
2020-06-14 10:53:35 +0300

    Add Login api test

---
+++ src/test/java/com/hillel/auto/selenide/example/api/LoginTest.java
@@ -0,0 +1,41 @@
+package com.hillel.auto.selenide.example.api;
+
+import com.hillel.auto.selenide.example.model.User;
+import com.hillel.auto.selenide.example.utils.UserData;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+/**
+ * Created by alpa on 5/17/20
+ */
+public class LoginTest {
+
+    private User user = UserData.defaultUser();
+
+    @Test
+    public void loginTest() {
+
+        RestAssured
+                .given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body("{\n"+
+                        "\"user\": {\n"+
+                        "\"email\": \""+user.getEmail()+"\",\n"+
+                        "\"password\": \""+user.getPassword()+"\"\n"+
+                        "}\n"+"}")
+                .when()
+                .post("https://conduit.productionready.io/api/users/login")
+                .then()
+                .statusCode(201)
+                .body("user.email", equalTo(user.getEmail().toLowerCase()))
+                .body("user.username", equalTo(user.getUsername()))
+                .body("user.token", notNullValue())
+                .log().all();
+    }
+
+}
